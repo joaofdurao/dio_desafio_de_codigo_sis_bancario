f7c3044f52cbe90ea38f3bba67ef74bb2c0edc03
joaofdurao <devjoaofelipe.dlc@gmail.com>
2024-04-29 16:24:27 -0300

    Final

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -5,60 +5,66 @@ limite_saque = 0
 saldo = 0.0
 
 def atualizar_saldo(deposita, valor):
+    """"Função para atualizar saldo bancário."""
+    
     global saldo
     
-    if deposita == True:
+    if deposita is True:
         saldo += valor
     else:
         saldo -= valor
     
-
 def depositar_valor(valor_depositar):
+    """"Função para depositar algum valor."""
+    
     if valor_depositar <= 0:
-        print("Valor de deposito não aceito.")
+        print("\n!! VALOR DE DEPOSITO NÃO ACEITO. !!\n")
+        
     else:
         historico_bancario.append("D: R$" + str(valor_depositar))
         historico_depositos.append(valor_depositar)
         atualizar_saldo(True, valor_depositar)
-        print(f"DEPOSITO DE R$ {valor_depositar} REALIZADO COM SUCESSO.")
+        print(f"\n!! DEPOSITO DE R$ {valor_depositar} REALIZADO COM SUCESSO. !!\n")
         
 def sacar_valor(valor_sacar):
+    """"Função para sacar algum valor."""
     global limite_saque
-    if limite_saque == 3:
-        print("Saque negado. Limite diário atingido.")
+    
+    if valor_sacar == 0.0:
+        print("\n!! SAQUE NEGADO. VALOR DE SAQUE INSUFICIENTE. !!\n")
         
-    elif saldo == 0.0:
-        print ("Saque negado. Saldo insuficiente.")
+    elif valor_sacar > 500.00:
+        print ("\n!! SAQUE NEGADO. LIMITE DE R$ 500,00 ATINGIDO. !!\n") 
+        
+    elif valor_sacar > saldo:
+        print ("\n!! SAQUE NEGADO. SALDO INSUFICIENTE. !!\n")
     
     else:
-        if valor_sacar > saldo:
-            print ("Saque negado. Saldo insuficiente.")
-        
-        elif valor_sacar > 500.00:
-            print ("Saque negado. Limite de R$500.00 atingido")
-            
-        else: 
-            historico_bancario.append("S: R$" + str(valor_sacar))
-            historico_saques.append(valor_sacar)
-            atualizar_saldo(False, valor_sacar)
-            print(f"SAQUE DE R$ {valor_sacar} REALIZADO COM SUCESSO.")
-            limite_saque += 1
+        historico_bancario.append("S: R$" + str(valor_sacar))
+        historico_saques.append(valor_sacar)
+        atualizar_saldo(False, valor_sacar)
+        print(f"\n!! SAQUE DE R$ {valor_sacar} REALIZADO COM SUCESSO. !!\n")
+        limite_saque += 1
 
-def imprimir_extrato():
+def imprimir_extrato():  
+    """"Função para imprimir extrato bancário.""" 
+    
     if len(historico_bancario) == 0:
-        print("Não foram realizadas movimentações.")
+        print("\n!! AINDA NÃO FORAM REALIZADAS MOVIMENTAÇÕES. !!\n")
     
     else:
-        print("----------------")
-        print("Extrato bancário")
-        print("----------------")
+        print("--------------------".center(79))
+        print("| Extrato bancário |".center(79))
+        print("--------------------".center(79))
         
         for i in historico_bancario:
-            print(i)    
+            print("".ljust(30) + "| " + i)    
      
-        print(f'SALDO TOTAL: R$ {saldo}')
+        print(''.ljust(30) + '| SALDO TOTAL: R$'+ str(saldo) + "\n")
 
 def inicio_view():
+    """Função de inícialização do programa."""
+    
     banco_dio = [
         " ------------------------------------------------------------------------------",
         " #######   #######  ##     ## ######## ########     #######  ######## ######## ",
@@ -75,6 +81,8 @@ def inicio_view():
         print(line)
 
 def menu_view():
+    """Função para menu do programa."""
+    
     menu_inicial = [
         "-----------------",
         "|   BANCO DIO   |",
@@ -87,17 +95,27 @@ def menu_view():
     ]
     
     for line in menu_inicial:
-        print(line)
+        print(line.center(79))
 
 def back_menu():
-    option = int(input("Escolha uma das opcoes acima: "))
+    """Função lógica para funcionamento do menu principal."""
+    
+    option = int(input(">>> Escolha uma das opcoes acima: "))
 
     if option == 1:
         deposito_view()
         return option
         
     elif option == 2:
-        saque_view()
+        if limite_saque == 3:
+            print("\n!! SAQUE NEGADO. LIMITE DIÁRIO ATINGIDO. !!\n")
+            
+        elif saldo == 0.0:
+            print("\n!! SAQUE NEGADO. SALDO EM CONTA INSUFICIENTE. !!\n")
+            
+        else:
+            saque_view()
+            
         return option
     
     elif option == 3:
@@ -108,19 +126,26 @@ def back_menu():
         return option
     
     else:
-        print("Opção invalida.")
+        print("\n!! OPÇÃO INVÁLIDA !!\n")
+        return option
         
 def deposito_view():
-    valor_deposito = float(input("Indique o valor a ser depositado: "))
+    """Função interativa para a operação de depósito."""
+    
+    valor_deposito = float(input(">>> Indique o valor a ser depositado: "))
     depositar_valor(valor_deposito)
     
     
 def saque_view():
-    valor_saque = float(input("Indique o valor a ser sacado: "))
+    """Função interativa para a operação de saque."""
+    
+    valor_saque = float(input(">>> Indique o valor a ser sacado: "))
     sacar_valor(valor_saque)
     
         
 def inicializar():
+    """Função para rodar o sistema."""
+    
     option = 0
     
     inicio_view()
