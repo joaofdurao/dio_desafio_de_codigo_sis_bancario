6862aa9a62f56953e1747c261e66f04c8b314dfc
joaofdurao <devjoaofelipe.dlc@gmail.com>
2024-07-02 16:45:46 -0300

    Iniciando controller

--- controllers/cliente_controller.py
+++ controllers/cliente_controller.py
@@ -2,16 +2,21 @@ from models.cliente import Cliente
 from repositories.cliente_repository import ClienteRepository
 
 class ClienteController:
-    def __init__(self):
-        self.cliente = Cliente()
-        self.repo = ClienteRepository()
 
-    def criar_cliente(self, nome, email):
-        cliente = {"nome": nome, "email": email}
-        self.clientes.append(cliente)
-        return cliente
+    def criar_cliente(self, nome=None, cpf=None, dt_nasc=None, logradouro=None, nr=None, bairro=None, cidade=None, estado=None):
+        endereco = f'{logradouro},{nr},{bairro},{cidade}/{estado}'
+        cliente = Cliente(nome = nome, cpf = cpf, dt_nasc = dt_nasc, endereco = endereco)
+        
+        try: 
+            ClienteRepository().create_cliente(cliente)
+            return True  
+        except Exception:
+            return False
 
-    def obter_cliente(self, nome):
+    def obter_cliente(self, nome=None, cpf=None, dt_nasc=None, endereco=None):
+        
+        
+        
         for cliente in self.clientes:
             if cliente["nome"] == nome:
                 return cliente
